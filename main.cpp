5179575257076ae0aec8a377e53b5acdc772c11b
e2380269 <e2380269@ceng.metu.edu.tr>
2023-04-14 01:30:47 +0300

    deneme

--- main.cpp
+++ main.cpp
@@ -1,24 +1,4 @@
-#include <cstdio>
-#include <cstdlib>
-#include <cstring>
-#include <string>
-#include <fstream>
-#include <iostream>
-#include <sstream>
-#include <vector>
-#define _USE_MATH_DEFINES
-#include <math.h>
-#include <GL/glew.h>
-#include <GLFW/glfw3.h> // The GLFW header
-#include <glm/glm.hpp> // GL Math library header
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
-//#include <glm/gtc/quaternion.hpp>
-//#include <glm/gtx/quaternion.hpp>
-
-#define BUFFER_OFFSET(i) ((char*)NULL + (i))
-
-using namespace std;
+#include "main.h"
 
 GLuint gProgram[2];
 int gWidth, gHeight;
@@ -34,40 +14,6 @@ glm::mat4 modelingMatrix;
 glm::vec3 eyePos(0, 0, 0);
 
 int activeProgramIndex = 0;
-
-struct Vertex
-{
-	Vertex(GLfloat inX, GLfloat inY, GLfloat inZ) : x(inX), y(inY), z(inZ) { }
-	GLfloat x, y, z;
-};
-
-struct Texture
-{
-	Texture(GLfloat inU, GLfloat inV) : u(inU), v(inV) { }
-	GLfloat u, v;
-};
-
-struct Normal
-{
-	Normal(GLfloat inX, GLfloat inY, GLfloat inZ) : x(inX), y(inY), z(inZ) { }
-	GLfloat x, y, z;
-};
-
-struct Face
-{
-	Face(int v[], int t[], int n[]) {
-		vIndex[0] = v[0];
-		vIndex[1] = v[1];
-		vIndex[2] = v[2];
-		tIndex[0] = t[0];
-		tIndex[1] = t[1];
-		tIndex[2] = t[2];
-		nIndex[0] = n[0];
-		nIndex[1] = n[1];
-		nIndex[2] = n[2];
-	}
-	GLuint vIndex[3], tIndex[3], nIndex[3];
-};
 
 vector<Vertex> gVertices;
 vector<Texture> gTextures;
---
+++ main.h
@@ -0,0 +1,51 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <vector>
+#define _USE_MATH_DEFINES
+#include <math.h>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+//#include <glm/gtc/quaternion.hpp>
+//#include <glm/gtx/quaternion.hpp>
+
+using namespace std;
+
+#define BUFFER_OFFSET(i) ((char*)NULL + (i))
+
+struct Vertex{
+    Vertex(GLfloat inX, GLfloat inY, GLfloat inZ) : x(inX), y(inY), z(inZ) { }
+    GLfloat x, y, z;
+};
+
+struct Texture{
+    Texture(GLfloat inU, GLfloat inV) : u(inU), v(inV) { }
+    GLfloat u, v;
+};
+
+struct Normal{
+    Normal(GLfloat inX, GLfloat inY, GLfloat inZ) : x(inX), y(inY), z(inZ) { }
+    GLfloat x, y, z;
+};
+
+struct Face{
+    Face(int v[], int t[], int n[]) {
+        vIndex[0] = v[0];
+        vIndex[1] = v[1];
+        vIndex[2] = v[2];
+        tIndex[0] = t[0];
+        tIndex[1] = t[1];
+        tIndex[2] = t[2];
+        nIndex[0] = n[0];
+        nIndex[1] = n[1];
+        nIndex[2] = n[2];
+    }
+    GLuint vIndex[3], tIndex[3], nIndex[3];
+};
