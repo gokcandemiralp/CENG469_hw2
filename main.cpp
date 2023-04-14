098d530501e3bd73221019e252af85addc4aa9a8
e2380269 <e2380269@ceng.metu.edu.tr>
2023-04-15 01:34:23 +0300

    got rid of the old parser

--- main.cpp
+++ main.cpp
@@ -11,134 +11,13 @@ GLint eyePosLoc;
 glm::mat4 projectionMatrix;
 glm::mat4 viewingMatrix;
 glm::mat4 modelingMatrix;
-glm::vec3 eyePos(0, 0, 0);
-
-vector<Vertex> gVertices;
-vector<Texture> gTextures;
-vector<Normal> gNormals;
-vector<Face> gFaces;
+glm::vec3 eyePos(0, 0, 2);
 
 GLuint gVertexAttribBuffer, gIndexBuffer;
-GLint gInVertexLoc, gInNormalLoc;
-int gVertexDataSizeInBytes, gNormalDataSizeInBytes;
+int gVertexDataSizeInBytes, gNormalDataSizeInBytes, indexDataSizeInBytes;
+int vertexEntries, normalEntries, faceEntries;
 
 fastObjMesh* m;
-
-bool ParseObj(const string& fileName){
-    fstream myfile;
-
-    // Open the input
-    myfile.open(fileName.c_str(), std::ios::in);
-
-    if (myfile.is_open()){
-        string curLine;
-
-        while (getline(myfile, curLine)){
-            stringstream str(curLine);
-            GLfloat c1, c2, c3;
-            string tmp;
-
-            if (curLine.length() >= 2){
-                if (curLine[0] == 'v'){
-                    if (curLine[1] == 't'){ // texture
-                        str >> tmp; // consume "vt"
-                        str >> c1 >> c2;
-                        gTextures.push_back(Texture(c1, c2));
-                    }
-                    else if (curLine[1] == 'n'){ // normal
-                        str >> tmp; // consume "vn"
-                        str >> c1 >> c2 >> c3;
-                        gNormals.push_back(Normal(c1, c2, c3));
-                    }
-                    else{ // vertex
-                        str >> tmp; // consume "v"
-                        str >> c1 >> c2 >> c3;
-                        gVertices.push_back(Vertex(c1, c2, c3));
-                    }
-                }
-                else if (curLine[0] == 'f'){ // face
-                    str >> tmp; // consume "f"
-                    char c;
-                    int vIndex[3], nIndex[3], tIndex[3];
-                    str >> vIndex[0]; str >> c >> c; // consume "//"
-                    str >> nIndex[0];
-                    str >> vIndex[1]; str >> c >> c; // consume "//"
-                    str >> nIndex[1];
-                    str >> vIndex[2]; str >> c >> c; // consume "//"
-                    str >> nIndex[2];
-
-                    assert(vIndex[0] == nIndex[0] &&
-                        vIndex[1] == nIndex[1] &&
-                        vIndex[2] == nIndex[2]); // a limitation for now
-
-                    // make indices start from 0
-                    for (int c = 0; c < 3; ++c){
-                        vIndex[c] -= 1;
-                        nIndex[c] -= 1;
-                        tIndex[c] -= 1;
-                    }
-
-                    gFaces.push_back(Face(vIndex, tIndex, nIndex));
-                }
-                else{
-                    cout << "Ignoring unidentified line in obj file: " << curLine << endl;
-                }
-            }
-            //data += curLine;
-            if (!myfile.eof()){
-                //data += "\n";
-            }
-        }
-
-        myfile.close();
-    }
-    else{
-        return false;
-    }
-
-    /*
-    for (int i = 0; i < gVertices.size(); ++i)
-    {
-        Vector3 n;
-
-        for (int j = 0; j < gFaces.size(); ++j)
-        {
-            for (int k = 0; k < 3; ++k)
-            {
-                if (gFaces[j].vIndex[k] == i)
-                {
-                    // face j contains vertex i
-                    Vector3 a(gVertices[gFaces[j].vIndex[0]].x,
-                              gVertices[gFaces[j].vIndex[0]].y,
-                              gVertices[gFaces[j].vIndex[0]].z);
-
-                    Vector3 b(gVertices[gFaces[j].vIndex[1]].x,
-                              gVertices[gFaces[j].vIndex[1]].y,
-                              gVertices[gFaces[j].vIndex[1]].z);
-
-                    Vector3 c(gVertices[gFaces[j].vIndex[2]].x,
-                              gVertices[gFaces[j].vIndex[2]].y,
-                              gVertices[gFaces[j].vIndex[2]].z);
-
-                    Vector3 ab = b - a;
-                    Vector3 ac = c - a;
-                    Vector3 normalFromThisFace = (ab.cross(ac)).getNormalized();
-                    n += normalFromThisFace;
-                }
-
-            }
-        }
-
-        n.normalize();
-
-        gNormals.push_back(Normal(n.x, n.y, n.z));
-    }
-    */
-
-    assert(gVertices.size() == gNormals.size());
-
-    return true;
-}
 
 bool ReadDataFromFile(
     const string& fileName, ///< [in]  Name of the shader file
@@ -256,10 +135,14 @@ void initVBO(){
     glBindBuffer(GL_ARRAY_BUFFER, gVertexAttribBuffer);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, gIndexBuffer);
 
-    gVertexDataSizeInBytes = m->position_count * 3 * sizeof(GLfloat);
-    gNormalDataSizeInBytes = m->normal_count * 3 * sizeof(GLfloat);
-    int indexDataSizeInBytes = m->face_count * 3 * sizeof(GLuint);
-    GLuint* indexData = new GLuint[gFaces.size() * 3];
+    vertexEntries = m->position_count * 3;
+    normalEntries = m->normal_count * 3;
+    faceEntries = m->face_count * 3;
+    
+    gVertexDataSizeInBytes = vertexEntries * sizeof(GLfloat);
+    gNormalDataSizeInBytes = normalEntries * sizeof(GLfloat);
+    indexDataSizeInBytes = faceEntries * sizeof(GLuint);
+    GLuint* indexData = new GLuint[faceEntries];
     
     for (int i = 0; i < m->face_count; ++i){
         indexData[3 * i] = m->indices[3 * i].p;
@@ -275,7 +158,6 @@ void initVBO(){
 
     // done copying; can free now
     delete[] indexData;
-    fast_obj_destroy(m);
 
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, BUFFER_OFFSET(gVertexDataSizeInBytes));
@@ -286,10 +168,6 @@ void init(){
     cout << "m->normal_count: " << m->normal_count << "\n" ;
     cout << "m->position_count: " << m->position_count << "\n" ;
     cout << "m->face_count: " << m->face_count << "\n" ;
-    // tinyObj o;
-    // bool success = read_tiny_obj("bunny.obj", &o);
-    
-    ParseObj("bunny.obj");
 
     glEnable(GL_DEPTH_TEST);
     initShaders();
@@ -303,7 +181,7 @@ void drawModel(){
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, BUFFER_OFFSET(gVertexDataSizeInBytes));
 
-    glDrawElements(GL_TRIANGLES, gFaces.size() * 3, GL_UNSIGNED_INT, 0);
+    glDrawElements(GL_TRIANGLES, faceEntries, GL_UNSIGNED_INT, 0);
 }
 
 void display(){
@@ -312,16 +190,10 @@ void display(){
     glClearStencil(0);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
 
-    static float angle = 0;
-
-    float angleRad = (float)(angle / 180.0) * M_PI;
-
     // Compute the modeling matrix
-    glm::mat4 matT = glm::translate(glm::mat4(1.0), glm::vec3(-0.1f, -0.2f, -7.0f));
-    glm::mat4 matRx = glm::rotate<float>(glm::mat4(1.0), (0. / 180.) * M_PI, glm::vec3(1.0, 0.0, 0.0));
-    glm::mat4 matRy = glm::rotate<float>(glm::mat4(1.0), (-180. / 180.) * M_PI, glm::vec3(0.0, 1.0, 0.0));
-    glm::mat4 matRz = glm::rotate<float>(glm::mat4(1.0), angleRad, glm::vec3(0.0, 0.0, 1.0));
-    modelingMatrix = matT * matRz * matRy * matRx;
+    glm::mat4 matT = glm::translate(glm::mat4(1.0), glm::vec3(0.0f, 0.0f, 0.0f));
+    glm::mat4 matS = glm::scale(glm::mat4(1.f), glm::vec3(0.2f ,0.2f ,0.2f));
+    modelingMatrix = matS * matT;
 
     // Set the active program and the values of its uniform variables
     glUseProgram(gProgram);
@@ -332,8 +204,6 @@ void display(){
 
     // Draw the scene
     drawModel();
-
-    angle += 0.5;
 }
 
 void reshape(GLFWwindow* window, int w, int h){
@@ -348,7 +218,7 @@ void reshape(GLFWwindow* window, int w, int h){
     float fovyRad = (float)(45.0 / 180.0) * M_PI;
     projectionMatrix = glm::perspective(fovyRad, w/(float) h, 1.0f, 100.0f);
 
-    viewingMatrix = glm::mat4(1);
+    viewingMatrix = viewingMatrix = glm::lookAt(eyePos,glm::vec3(0,0,0),glm::vec3(0,1,0));
 }
 
 void keyboard(GLFWwindow* window, int key, int scancode, int action, int mods){
