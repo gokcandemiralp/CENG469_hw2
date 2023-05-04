7dc45a97aaae51578f37a76c45dee1a62f704afe
e2380269 <e2380269@ceng.metu.edu.tr>
2023-05-04 19:11:48 +0300

    better reflections b

---
+++ shaders/vehicleFrag.glsl
@@ -0,0 +1,25 @@
+#version 330 core
+
+in vec2 TexCoord;
+in vec3 Normal;
+in vec4 specular;
+in vec3 Position;
+
+layout (std140) uniform Matrices{
+    mat4 projectionMatrix;
+    mat4 viewingMatrix;
+    vec3 eyePos;
+};
+
+out vec4 fragColor;
+
+uniform sampler2D sampler;
+uniform samplerCube skybox;
+
+void main(void){
+    vec3 I = normalize(Position-eyePos);
+    vec3 R = reflect(I, normalize(Normal));
+    fragColor = vec4(texture(skybox, R));
+    // fragColor = texture(sampler, TexCoord) + specular;
+    // fragColor = vec4(normal,1);
+}
